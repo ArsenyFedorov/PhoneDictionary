b9cb2c8b6325f01f49a75d8439c3c46fab9fbbdc
Арсений <ars071026@example.com>
2023-06-16 15:47:40 +0500

    PhoneDictionary

--- console.py
+++ console.py
@@ -13,32 +13,10 @@ def user_event():
         print("=" * len(event_error_69))
 
 
-def user_event_1():
-    # Эта функция сообщает о завершении 1 события
-    print("=" * 22)
-    print(event_1)
-    print("=" * 22)
-
-
-def error_1():
-    # Эта функция сообщает об ошибке 1 события
-    print("=" * 11)
-    print(error_event_1)
-    print("=" * 11)
-
-
-def user_event_2():
-    # Эта функция сообщает о завершении 2 события
-    print("=" * len(event_2))
-    print(event_2)
-    print("=" * len(event_2))
-
-
-def error_2():
-    # Эта функция сообщает об ошибке 2 события
-    print("=" * len(error_event_2))
-    print(error_event_2)
-    print("=" * len(error_event_2))
+def print_message(message: str):
+    print("=" * len(message))
+    print(message)
+    print("=" * len(message))
 
 
 def user_event_3(book: list[dict[str, str]]):
@@ -68,45 +46,10 @@ class NewUser:
         self.comment = input(new_comment)
 
 
-def user_event_4():
-    # Эта функция сообщает о том что контакт создан
-    print("=" * len(event_4))
-    print(event_4)
-    print("=" * len(event_4))
-
-
-def error_4():
-    # Эта функция сообщает о том что файл не открыт
-    print("=" * len(error_event_4))
-    print(error_event_4)
-    print("=" * len(error_event_4))
-
-
-def error_input():
-    # Эта функция сообщает об ошибке когда пользователь ничего не ввёл в поле имя или номер
-    print("=" * len(name_phone_error))
-    print(name_phone_error)
-    print("=" * len(name_phone_error))
-
-
 def user_event_5():
     # Эта функция собирает искомые данные
     element = input(event_5)
     return element
-
-
-def error_5():
-    # Эта функция сообщает о том что файл пустой или не открыт
-    print("=" * len(event_error_5))
-    print(event_error_5)
-    print("=" * len(event_error_5))
-
-
-def error_55():
-    # Эта функция сообщает об отсутствие искомых данных
-    print('=' * len(event_error_55))
-    print(event_error_55)
-    print('=' * len(event_error_55))
 
 
 def user_event_6():
@@ -134,58 +77,9 @@ def user_variation():
     return u_variation
 
 
-def f_event_6():
-    # Эта функция сообщает об окончании 6 события
-    print("=" * len(event_6_finish))
-    print(event_6_finish)
-    print("=" * len(event_6_finish))
-
-
-def error_6():
-    # Эта функция сообщает что контакта который ввёл пользователь нет
-    print("=" * len(event_error_6))
-    print(event_error_6)
-    print("=" * len(event_error_6))
-
-
-def error_66():
-    # Эта функция сообщает о том что файл пустой
-    print("=" * len(event_error_66))
-    print(event_error_66)
-    print("=" * len(event_error_66))
-
-
-def error_69():
-    # Эта функция сообщает о том что такой функции нет
-    print("=" * len(event_error_69))
-    print(event_error_69)
-    print("=" * len(event_error_69))
-
-
-def var_error():
-    # Эта функция сообщает о том что пользователь ничего не изменил
-    print("=" * len(variation_error))
-    print(variation_error)
-    print("=" * len(variation_error))
-
-
 def user_event_7():
     # Эта функция запрашивает данные для удаления
     print("=" * len(event_7))
     del_user = input(event_7)
     print("=" * len(event_7))
     return del_user
-
-
-def f_event_7():
-    # Эта команда сообщает о завершении 7 функции
-    print("=" * len(event_7_finish))
-    print(event_7_finish)
-    print("=" * len(event_7_finish))
-
-
-def error_7():
-    # Эта функция сообщает об ошибке в 7 функции
-    print("=" * len(event_error_7))
-    print(event_error_7)
-    print("=" * len(event_error_7))
--- control.py
+++ control.py
@@ -1,6 +1,6 @@
 from console import *
 from func import *
-
+from text import *
 
 def start():
     while True:
@@ -9,72 +9,72 @@ def start():
         match choice:
             case 1:
                 if check == "":
-                    error_1()
+                    print_message(error_event_1)
                 else:
                     open_file()
-                    user_event_1()
+                    print_message(event_1)
             case 2:
                 if phone_list == []:
-                    error_2()
+                    print_message(error_event_2)
                 else:
                     save_file()
-                    user_event_2()
+                    print_message(event_2)
             case 3:
                 user_event_3(phone_list)
             case 4:
                 if check != "" and phone_list == []:
-                    error_4()
+                    print_message(error_event_4)
                 else:
                     new_user = NewUser()
                     if new_user.name == "" or new_user.phone == "":
-                        error_input()
+                        print_message(name_phone_error)
                     else:
                         new_contact(new_user.name, new_user.phone, new_user.comment)
-                        user_event_4()
+                        print_message(event_4)
             case 5:
                 if check == "" or phone_list == []:
-                    error_5()
+                    print_message(event_error_5)
                 else:
                     search_list = search_contact(user_event_5())
                     if search_list == []:
-                        error_55()
+                        print_message(event_error_55)
                     else:
                         user_event_3(search_list)
             case 6:
                 if phone_list == []:
-                    error_66()
+                    print_message(event_error_66)
                 else:
                     user_id = user_event_6()
                     if not user_id.isdigit():
-                        error_6()
+                        print_message(event_error_6)
                     elif int(user_id) > int(phone_list[-1].get("id")):
-                        error_6()
+                        print_message(event_error_6)
                     else:
                         command = change_event()
                         if not command.isdigit():
-                            error_69()
+                            print_message(event_error_69)
                         elif 0 > int(command) or int(command) > 4:
-                            error_69()
+                            print_message(event_error_69)
                         elif int(command) == 4:
                             continue
                         else:
                             u_variation = user_variation()
                             if u_variation == "":
-                                var_error()
+                                print_message(variation_error)
                             else:
                                 user_var(user_id, command, u_variation)
-                                f_event_6()
+                                print_message(event_6_finish)
             case 7:
                 if check == "" or phone_list == []:
-                    error_5()
+                    print_message(event_error_5)
                 else:
                     user_del = user_event_7()
                     if not user_del.isdigit():
-                        error_7()
+                        print_message(event_error_7)
                     elif int(user_del) > int(phone_list[-1].get("id")):
-                        error_7()
+                        print_message(event_error_7)
                     else:
                         del_user(user_del)
-                        f_event_7()
+                        print_message(event_7_finish)
             case 8:
                 break
